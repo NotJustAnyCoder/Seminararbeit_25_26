466973573c37cca590f0d79a7645d1171be31ab9
Maximilian Seiler <maximilian.carl.seiler@gmail.com>
2025-10-14 21:06:22 +0200

    Update SimpleFOC.ino

--- SimpleFOC/SimpleFOC.ino
+++ SimpleFOC/SimpleFOC.ino
@@ -4,20 +4,34 @@
 // Config magnetic encoder
 int mag_enc_pin = 1;
 int mag_enc_res = 12;
-
 #define angle_reg 0x0E;
-
 MagneticSensorSPI mag_enc (mag_enc_pin, mag_enc_res, angle_reg)
 
-// Config BLDC
+// Config BLDC Driver
 // ⚠️ Only PWM Pins
 int phase_1 = 9;
 int phase_2 = 10;
 int phase_3 = 11;
-
 int bldc_voltage; //External battery
-
 BLDCDriver3PWM bldc_driver = BLDCDriver3PWM(phase_1, phase_2, phase_3);
+
+// Config BLDC
+// XING-E Motor
+int xing_pole_pairs = 7; //14 Poles
+int xing_phase_resistance = 1;
+int xing_kv = 1800;
+
+// Badu Motor
+int badu_pole_pairs = 7 //14 Poles
+int badu_phase_resistance = 1;
+int badu_kv = 360;
+
+// Ufo Motor
+int jufo_pole_paris = 0;
+int ufo_phase_resistance = 0;
+int ufo_kv = 0;
+
+BLDCMotor bldc_motor = BLDCMotor(xing_pole_pairs, xing_phase_resistance, xing_kv);
 
 void setup() {
   // Encoder Setup
@@ -28,6 +42,9 @@ void setup() {
   bldc_driver.voltage_power_supply = bldc_voltage;
   bldc_driver.voltage_limit = 35;
   bldc_driver.init()
+
+  // BLDC Motor Setup
+  
 }
 
 void loop() {
