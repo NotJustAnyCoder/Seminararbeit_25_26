997b6c3876d3c6fe9d2d872cd6372d1a30390206
Maximilian Seiler <maximilian.carl.seiler@gmail.com>
2025-10-14 19:35:14 +0200

    Update Code1.ino

--- Code1/Code1.ino
+++ Code1/Code1.ino
@@ -1,4 +1,4 @@
-// Shows angle of magnetic encoder and status
+// Shows angle of magnetic encoder and status, to show status enter something in the command line
 #include <Wire.h>
 
 #define mag_enc_add 0x36
@@ -28,23 +28,18 @@ void loop() {
     }
   }
 
+  Wire.write(angle_add);
+  Wire.endTransmission(false);
+  Wire.requestFrom(mag_enc_add, 2);
+
+  if(Wire.available() == 2){
+
+  }
+  
+
   delay(100);
 
-  /*
-  Wire.beginTransmission(mag_enc_add);
 
-  if(Serial.available() == 1){
-    Wire.write(status_add);
-    Wire.requestFrom(mag_enc_add, 1);
-
-    if(Wire.available()==1){
-      int byte_val = Wire.read();
-      Serial.println("Status_val: " + String(byte_val));
-    }
-  }
-  `*/
-  
 }
-
 
 //Made by Mäx
