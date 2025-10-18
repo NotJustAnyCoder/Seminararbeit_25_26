923a8430d5e3ae86937701df8c31a46827aa32c3
Maximilian Seiler <maximilian.carl.seiler@gmail.com>
2025-10-18 21:25:42 +0200

    Update OpenLoop.ino

--- OpenLoop/OpenLoop.ino
+++ OpenLoop/OpenLoop.ino
@@ -1,20 +1,33 @@
 #include <SimpleFOC.h>
 
-// Variable Setup
-
 // Motor Setup
+float motor_voltage = 1.00;
+float align_voltage = 3.00;
+int num_magnet_pairs = 7;
+
+BLDCMotor bldc_motor = BLDCMotor(num_magnet_pairs);
 
 
 // Driver Setup
+float driver_voltage = 15;
+
+BLDCDriver3PWM bldc_driver = BLDCDriver3PWM(2, 3 ,4 ,6);  // With RPI Pico W, I used the EN pin
+bldc_driver.pwm_frequency = 20000;
+bldc_driver.voltage_power_supply = 15;
+bldc_driver.voltage_limit = 12;
+
 
 void setup() {
-  // put your setup code here, to run once:
-
+  // Motor Startup
+  bldc_motor.linkDriver(&bldc_driver);
+  bldc_motor.voltage_align_sensor = align_voltage;
+  bldc_motor.voltage_limit = motor_voltage;
+  bldc_motor.controller = MotionControlType::velocity_openloop;
+  bldc_motor.init();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  bldc_motor.move();
 }
 
 //Made by Mäx
