51b7241c4e69fedc75f4dc708935f1ca3f60dae7
Maximilian Seiler <maximilian.carl.seiler@gmail.com>
2025-10-18 21:06:56 +0200

    Create OpenLoop.ino

---
+++ OpenLoop/OpenLoop.ino
@@ -0,0 +1,20 @@
+#include <SimpleFOC.h>
+
+// Variable Setup
+
+// Motor Setup
+
+
+// Driver Setup
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+//Made by Mäx
