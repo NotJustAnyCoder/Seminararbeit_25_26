07783b4f35e193af3590b309bd677fffb9294224
Maximilian Seiler <maximilian.carl.seiler@gmail.com>
2025-10-18 21:40:41 +0200

    Update OpenLoop.ino

--- OpenLoop/OpenLoop.ino
+++ OpenLoop/OpenLoop.ino
@@ -16,18 +16,31 @@ bldc_driver.pwm_frequency = 20000;
 bldc_driver.voltage_power_supply = 15;
 bldc_driver.voltage_limit = 12;
 
+// Move Setup
+move_value = 0;
 
 void setup() {
+  Serial.begin(12500);
+
   // Motor Startup
   bldc_motor.linkDriver(&bldc_driver);
   bldc_motor.voltage_align_sensor = align_voltage;
   bldc_motor.voltage_limit = motor_voltage;
   bldc_motor.controller = MotionControlType::velocity_openloop;
   bldc_motor.init();
+  
+  // Driver Startup
+  SimpleFOCDebug::enable(%Serial);
+  bldc_driver.init();
+  bldc_driver.enable();
+
+  Serial.println("Setup finished: Running Loop");
+  Serial.println("Type Mx to move the motor.");
+
 }
 
 void loop() {
-  bldc_motor.move();
+  bldc_motor.move(move_value);
 }
 
 //Made by Mäx
