a6f95470b4ad0d9b0f526170e0095f182915f62f
Maximilian Seiler <maximilian.carl.seiler@gmail.com>
2025-10-24 21:29:30 +0200

    Update ClosedLoopVelocity.ino

--- ClosedLoopVelocity/ClosedLoopVelocity.ino
+++ ClosedLoopVelocity/ClosedLoopVelocity.ino
@@ -75,14 +75,3 @@ void driver_volt(char* driver_voltage_comms){
 }
 
 //Made by Mäx
-
-
-void setup() {
-  // put your setup code here, to run once:
-
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
